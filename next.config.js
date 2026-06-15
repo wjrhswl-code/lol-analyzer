343bc9bec5b357d71afad9f6ccf65a9ca42e090a
wjrhswl-code <GitHubwjrhswl@gmail.com>
2026-06-15 11:56:19 +0900

    fix env config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   images: {
     domains: ['ddragon.leagueoflegends.com', 'raw.communitydragon.org'],
   },
-  env: {
-    RIOT_API_KEY: process.env.RIOT_API_KEY,
-    ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY,
-  },
 }
 
 module.exports = nextConfig
